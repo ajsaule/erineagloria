80c0b742e134c96512319be04971a7377e4d8879
Erinea Gloria <erineagloria@gmail.com>
2020-09-30 15:23:19 +1000

    Updates

---
+++ precache-manifest.043e8e6dceebcdf44df023c1deb1cb02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1414e54404daba6d84ded63eeea3eb32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cacef4064586aeb00e00",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "826a6a2885051bab646b",
+    "url": "/static/css/main.bd3a6ce4.chunk.css"
+  },
+  {
+    "revision": "cacef4064586aeb00e00",
+    "url": "/static/js/2.1c7d5d1a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1c7d5d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "826a6a2885051bab646b",
+    "url": "/static/js/main.87fbe19b.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  }
+]);
